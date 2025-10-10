2563348bf9a4d49336ea6ce11de5814cf3fcc5d3
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-10-10 20:46:21 -0300

    refactor: change database seed in api

--- api/src/infra/database/drizzle/seed/seed.ts
+++ api/src/infra/database/drizzle/seed/seed.ts
@@ -1,33 +1,49 @@
-import { reset, seed } from 'drizzle-seed'
+import { faker } from '@faker-js/faker'
 import { db } from '../client.ts'
 import { schema } from '../schema/index.ts'
 import { bookCategories } from './book-categories.ts'
 import { books } from './books.ts'
 import { categories } from './categories.ts'
 
-await reset(db, schema)
+/** Reset database */
+for await (const table of Object.values(schema)) {
+  await db.delete(table)
+}
 
 await db.insert(schema.books).values(books)
 await db.insert(schema.categories).values(categories)
 await db.insert(schema.bookCategories).values(bookCategories)
 
-const booksIds = books.map((book) => book.id)
-const ratings = Array.from({ length: 10 }).map((_, index) => (index + 1) / 2)
+const users = await db
+  .insert(schema.users)
+  .values(
+    Array.from({ length: 10 }).map(() => {
+      return {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        avatarUrl: faker.image.avatarGitHub(),
+      }
+    }),
+  )
+  .returning()
 
-await seed(db, {
-  users: schema.users,
-  ratings: schema.ratings,
-}).refine((funcs) => {
-  return {
-    ratings: {
-      count: 100,
-      columns: {
-        bookId: funcs.valuesFromArray({ values: booksIds }),
-        rating: funcs.valuesFromArray({ values: ratings }),
-      },
-    },
-  }
-})
+const booksIds = books.map((book) => book.id)
+const usersIds = users.map((user) => user.id)
+
+const RATING_VALUES = Array.from({ length: 10 }).map(
+  (_, index) => (index + 1) / 2,
+)
+
+await db.insert(schema.ratings).values(
+  Array.from({ length: 100 }).map(() => {
+    return {
+      bookId: faker.helpers.arrayElement(booksIds),
+      userId: faker.helpers.arrayElement(usersIds),
+      description: faker.lorem.paragraph(),
+      rating: faker.helpers.arrayElement(RATING_VALUES),
+    }
+  }),
+)
 
 await db.$client.end()
 
