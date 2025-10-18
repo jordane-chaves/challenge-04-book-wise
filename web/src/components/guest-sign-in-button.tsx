653f22da49d9c2f9c5e3e3fa655ba10478d409a9
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-10-18 09:09:41 -0300

    fix: add guest sign in button action in web

--- web/src/components/guest-sign-in-button.tsx
+++ web/src/components/guest-sign-in-button.tsx
@@ -1,9 +1,18 @@
+"use client"
+
 import { RocketLaunchIcon } from "@phosphor-icons/react/dist/ssr"
+import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
 
 export function GuestSignInButton() {
+  const router = useRouter()
+
+  function handleNavigateToHome() {
+    router.push("/")
+  }
+
   return (
-    <Button size="lg">
+    <Button size="lg" onClick={handleNavigateToHome}>
       <RocketLaunchIcon className="size-8 text-primary" />
       Acessar como Visitante
     </Button>
